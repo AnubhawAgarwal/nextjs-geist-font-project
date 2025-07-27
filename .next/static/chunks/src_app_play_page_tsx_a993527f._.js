7c5abb796d638a27e0dd39c25e5b1d1a2e9f0bf9
Anubhaw Agarwal <agarwal.anubhaw8909@gmail.com>
2025-07-27 18:09:07 +0530

    Add .next/static/chunks/src_app_play_page_tsx_a993527f._.js 1753619946466

---
+++ .next/static/chunks/src_app_play_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_0507319a._.js",
+  "static/chunks/src_236e8012._.js"
+],
+    source: "dynamic"
+});
